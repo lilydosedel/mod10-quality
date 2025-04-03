3152ba1ee3b60f8afd6a5dfce40096099be5469b
Lily Dosedel <lilydosedelcs@gmail.com>
2025-04-03 14:34:06 -0500

    write initial commit

---
+++ bad.py
@@ -0,0 +1,9 @@
+""" This module is used to add the square of two numbers"""
+import math
+
+def messy_func(x, y):
+    """ This module is used to add the square of two numbers"""
+    return math.pow(x, 2) + math.pow(y, 2)
+
+
+print(messy_func(3, 4))
